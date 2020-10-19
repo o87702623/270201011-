584c566df9ba0e807f22237f8c4c99526453ddd0
o87702623 <rumeysacoskun@std.iyte.edu.tr>
2020-10-19 09:20:42 +0000

    .

--- lab1/example3.py
+++ lab1/example3.py
@@ -1,6 +1,9 @@
-question = a*x**2 + b*x + c
+
 a=2
 b=6
 c=-20
-solution= (-1*b -+ (b**2 - 4*a*c)**0.5) / 2*a
-print(solution)
+d= b**2 - 4*a*c
+solution1= (-1*b - d**0.5) / 2*a
+solution2= (-1*b + d**0.5)
+print(solution1)
+print(solution2)
